3075f49d584710fabc2824d11a2a18710672402d
Qing <592423741@qq.com>
2017-10-25 13:41:41 +0800

    完成人机对战

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ var chessBoard = [];
 var wins =[];
 
 //赢法统计数组
-var mywin =[];
-var computerwin =[];
+var myWin =[];
+var computerWin =[];
 
 //赢法个数
 var count = 0;
@@ -78,8 +78,8 @@ for (var i = 0; i < 11; i++) {
 }
 
 for(var i = 0;i<count;i++){
-	mywin[i]=0;
-	computerwin[i] =0;
+	myWin[i]=0;
+	computerWin[i] =0;
 }
 
 
@@ -122,27 +122,113 @@ chess.onclick = function(e){
 	if(over){
 		return;
 	}
+	if(!me){
+		return;
+	}
 	var x = e.offsetX;
 	var y = e.offsetY;
 	i = Math.floor(x / (2*20));  
     j = Math.floor(y / (2*20)); 
     if(chessBoard[i][j] == 0){
 	    onestep(i, j, me); 
-    	if(me){		
-    		chessBoard[i][j] = 1;
-    	}else{
-    		chessBoard[i][j] = 2;
-    	}
-    	me = !me;
+    	chessBoard[i][j] = 1;
     	for(var k =0 ;k<count;k++){
     		if(wins[i][j][k]){
-    			mywin[k]++;
-    			computerwin[k] = -1;
-    			if(mywin[k] ==5){
+    			myWin[k]++;
+    			computerWin[k] = -1;
+    			if(myWin[k] ==5){
     				window.alert('你赢了！');
     				over = true;
     			}
     		}
     	}
+    	if(!over){
+    		me = !me;
+    		setTimeout(computerAI, 100);
+    	}
     }
+}
+
+//计算机AI
+function computerAI(){
+	var myScore =[];
+	var computerScore =[];
+	//保存最高分数
+    var max = 0;
+    //最高分数坐标
+    var u = 0, v = 0;
+    //初始化数组
+	for(var i = 0; i<15;i++){
+		myScore[i]=[];
+		computerScore[i]=[];
+		for(var j = 0;j<15;j++){
+			myScore[i][j]=0;
+			computerScore[i][j]=0;
+		}
+	}
+	for (var i = 0; i < 15; i++) {
+        for (var j = 0; j < 15; j++) {
+            if (chessBoard[i][j] == 0) {
+                for (var k = 0; k < count; k++) {
+                    if (wins[i][j][k]) {
+                        if (myWin[k] == 1) {
+                            //第K种赢法 黑棋(玩家)落了第一颗子
+                            myScore[i][j] += 200;
+                        } else if (myWin[k] == 2) {
+                            myScore[i][j] += 400;
+                        } else if (myWin[k] == 3) {
+                            myScore[i][j] += 2000;
+                        } else if (myWin[k] == 4) {
+                            myScore[i][j] += 10000;
+                        }
+                        if (computerWin[k] == 1) {
+                            //第K种赢法 白棋(电脑)落了第一颗子
+                            computerScore[i][j] += 200;
+                        } else if (computerWin[k] == 2) {
+                            computerScore[i][j] += 420;
+                        } else if (computerWin[k] == 3) {
+                            computerScore[i][j] += 2100;
+                        } else if (computerWin[k] == 4) {
+                            computerScore[i][j] += 20000;
+                        }
+                    }
+                }
+                if (myScore[i][j] > max) {
+                    max = myScore[i][j];
+                    u = i;
+                    v = j;
+                } else if (myScore[i][j] == max) {
+                    if (computerScore[i][j] > computerScore[u][v]) {
+                        u = i;
+                        v = j;
+                    }
+                }
+                if (computerScore[i][j] > max) {
+                    max = computerScore[i][j];
+                    u = i;
+                    v = j;
+                } else if (computerScore[i][j] == max) {
+                    if (computerScore[i][j] > myScore[u][v]) {
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+        }
+    }
+    onestep(u,v,0);
+    chessBoard[u][v] =2;
+    for(var k =0 ;k<count;k++){
+		if(wins[u][v][k]){
+			computerWin[k]++;
+			myWin[k] = -1;
+			if(computerWin[k] ==5){
+				window.alert('计算机赢了！');
+				over = true;
+			}
+		}
+	}
+	if(!over){
+		me =!me;
+	}
 }
