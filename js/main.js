fbf4941052d4560d1539d6896e3e7f2b21d466a3
Qing <592423741@qq.com>
2017-10-25 14:10:39 +0800

    完成开始按钮

--- js/main.js
+++ js/main.js
@@ -10,9 +10,15 @@ chess.height = 600;
 document.body.appendChild( chess );
 var ctx = chess.getContext('2d');  
 
-//判断是玩家还是电脑
+//me:true:黑棋(玩家) false:白棋
 var me = true;
 var over = false;
+
+//棋盘上是否有子
+var haveChess = false;
+//是否点击了开始按钮
+var startBtn = false;
+
 //存储棋盘落子情况
 //0:无子 1:黑棋 2:白棋
 var chessBoard = [];
@@ -27,62 +33,72 @@ var computerWin =[];
 //赢法个数
 var count = 0;
 
-//初始化chessBoard、wins数组
-for(var i= 0; i<15;i++){
-	chessBoard[i]=[];
-	wins[i] =[];
-	for(var j = 0;j<15;j++){
-		chessBoard[i][j] =0;
-		wins[i][j]=[];
+window.onload = function () {
+    drawBoard(600,20);
+};
+
+function newGame(){
+	//擦除画布
+	ctx.clearRect(0, 0, chess.width, chess.height);
+    drawBoard(600,20);
+    //初始化变量
+    me = true;
+    over = false;
+    startClick = false;
+    gameStart = true;
+    count = 0;
+    //初始化chessBoard、wins数组,wins[][]棋盘坐标
+	for(var i= 0; i<15;i++){
+		chessBoard[i]=[];
+		wins[i] =[];
+		for(var j = 0;j<15;j++){
+			chessBoard[i][j] =0;
+			wins[i][j]=[];
+		}
 	}
+	//所有竖线赢法|
+	for (var i = 0; i < 15; i++) {  
+	 for (var j = 0; j < 11; j++) {  
+	     for (var k = 0; k < 5; k++) {  
+	         wins[i][j + k][count] = true;  
+	     }  
+	     count++;  
+	 }  
+	}  
+	//所有横线线赢法-
+	for (var i = 0; i < 15; i++) {  
+	 for (var j = 0; j < 11; j++) {  
+	     for (var k = 0; k < 5; k++) {  
+	         wins[j + k][i][count] = true;  
+	     }  
+	     count++;  
+	 }  
+	}  
+	//所有斜线赢法 /
+	for (var i = 0; i < 11; i++) {  
+	 for (var j = 0; j <11 ; j++) {  
+	     for (var k = 0; k < 5; k++) {  
+	         wins[i + k][j + k][count] = true;  
+	     }  
+	     count++;  
+	 }  
+	}  
+	//所有反斜线赢法 \
+	for (var i = 0; i < 11; i++) {  
+	 for (var j = 14; j > 3; j--) {  
+	     for (var k = 0; k < 5; k++) {  
+	         wins[i + k][j - k][count] = true;  
+	     }  
+	     count++;  
+	 }  
+	}
+	//初始化黑白棋落子赢法
+	for(var i = 0;i<count;i++){
+		myWin[i]=0;
+		computerWin[i] =0;
+	}
+
 }
-
-
-drawBoard(600,20);
-
-//所有竖线赢法|
-for (var i = 0; i < 15; i++) {  
- for (var j = 0; j < 11; j++) {  
-     for (var k = 0; k < 5; k++) {  
-         wins[i][j + k][count] = true;  
-     }  
-     count++;  
- }  
-}  
-//所有横线线赢法-
-for (var i = 0; i < 15; i++) {  
- for (var j = 0; j < 11; j++) {  
-     for (var k = 0; k < 5; k++) {  
-         wins[j + k][i][count] = true;  
-     }  
-     count++;  
- }  
-}  
-//所有斜线赢法 /
-for (var i = 0; i < 11; i++) {  
- for (var j = 0; j <11 ; j++) {  
-     for (var k = 0; k < 5; k++) {  
-         wins[i + k][j + k][count] = true;  
-     }  
-     count++;  
- }  
-}  
-//所有反斜线赢法 \
-for (var i = 0; i < 11; i++) {  
- for (var j = 14; j > 3; j--) {  
-     for (var k = 0; k < 5; k++) {  
-         wins[i + k][j - k][count] = true;  
-     }  
-     count++;  
- }  
-}
-
-for(var i = 0;i<count;i++){
-	myWin[i]=0;
-	computerWin[i] =0;
-}
-
-
 
 //画棋盘
 function  drawBoard(w,r){
@@ -96,7 +112,7 @@ function  drawBoard(w,r){
 		ctx.lineTo(r + i * 2*r, w-r);  
 	}  
 	ctx.stroke();  
-	ctx.strokeStyle = "#ccc";
+	// ctx.strokeStyle = "#ccc";
 	ctx.closePath();
 }
 
@@ -117,8 +133,17 @@ function onestep(i, j, me) {
     ctx.fill();  
     ctx.closePath();  
 }  
-//玩家落子
-chess.onclick = function(e){
+//玩家落子事件
+chess.onclick = function (e) {
+    myClick(e);
+}
+//落子
+function myClick(e){
+	if (startBtn) {
+        haveChess = true;
+    } else if (!startBtn && !haveChess) {
+        return;
+    }
 	if(over){
 		return;
 	}
@@ -137,8 +162,10 @@ chess.onclick = function(e){
     			myWin[k]++;
     			computerWin[k] = -1;
     			if(myWin[k] ==5){
-    				window.alert('你赢了！');
     				over = true;
+    				setTimeout(function () {
+                   		gameOver();
+                	}, 100)
     			}
     		}
     	}
@@ -223,12 +250,42 @@ function computerAI(){
 			computerWin[k]++;
 			myWin[k] = -1;
 			if(computerWin[k] ==5){
-				window.alert('计算机赢了！');
-				over = true;
+    			over = true;
+				setTimeout(function () {
+                    gameOver();
+                }, 100)
 			}
 		}
 	}
 	if(!over){
 		me =!me;
 	}
+}
+
+//结束函数
+function gameOver(me) {
+    if (me) {
+        if (window.confirm('恭喜你，击败了电脑，是否重新开始？')) {
+            newGame();
+        }
+    } else {
+        if (window.confirm('电脑赢了，是否重新开始？')) {
+            newGame();
+        }
+    }
+}
+
+//点击开始按钮
+start.onclick = function () {
+    startBtn = true;
+    if (haveChess && startBtn) {
+        if (window.confirm('是否重新开始游戏？')) {
+            newGame();
+            haveChess = false;
+        }
+    } else {
+        newGame();
+        haveChess = false;
+    }
+
 }
